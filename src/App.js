d7f57189ac5f6ae3cc9e665ca219050af1ec991e
lee cheong woo <86641797+Nangnaa@users.noreply.github.com>
2022-03-30 11:06:40 +0900

    '등록했습니다.'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+    return (
+      <div>
+        <Header>
+          <div className='headerIn'>
+            <Navigation/>
+            <div className='profile'></div>
+          </div>
+          {/* 1페이지
+            네비게이션(프로필 html~react 기술기입 인적사항 깃이나블로그주소 )
+          */}
+
+          <div className='mainImg'></div>
+          <Contents>
+            <con1/>
+            <con2/>
+            <con3/>
+          </Contents>
+          {/*2페이지
+           프로젝트들을 보여주는 페이지 */}
+
+          <div className='footer'>
+            <div></div>
+          </div>
+          {/* 3페이지 이메일이나 연락처 크게확대 */}
+        </Header>
+      </div>
+          
+    )
+          
+       
+    
+  }  
+
+
 
 export default App;
+
+ 
